4890b7fdb6fccf91ced554a1c395fa2e59af7f07
techyguyperplexable <154611145+techyguyperplexable@users.noreply.github.com>
2025-04-26 21:02:24 +0100

    Partition remap

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,7 @@ TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64
 TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := cortex-a76
-TARGET_CPU_VARIANT_RUNTIME := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := oriole
 TARGET_NO_BOOTLOADER := true
@@ -39,16 +39,38 @@ endif
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 67108864
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
+BOARD_SUPER_PARTITION_SIZE := 9126805504 # TODO: Fix hardcoded value
+BOARD_SUPER_PARTITION_GROUPS := google_dynamic_partitions
+BOARD_GOOGLE_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product vendor vendor_dlkm
+BOARD_GOOGLE_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
 
+VENDOR_CMDLINE := "dyndbg=\"func alloc_contig_dump_pages +p\" \
+                earlycon=exynos4210,0x10A00000 console=ttySAC0,115200 androidboot.console=ttySAC0 printk.devkmsg=on \
+                swiotlb=noforce \
+		cma_sysfs.experimental=Y \
+		cgroup_disable=memory \
+		rcupdate.rcu_expedited=1 \
+		rcu_nocbs=all \
+		stack_depot_disable=off \
+		page_pinner=on \
+		swiotlb=1024 \
+		disable_dma32=on \
+                at24.write_timeout=100 \
+		log_buf_len=1024K \
+		bootconfig"
+
+BOARD_BOOTCONFIG += androidboot.load_modules_parallel=true
 # Platform
-TARGET_BOARD_PLATFORM := unknown
+TARGET_BOARD_PLATFORM := gs101
 
 # Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -62,5 +84,8 @@ TW_MAX_BRIGHTNESS := 100
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
-PLATFORM_VERSION := 16.1.0
+PLATFORM_VERSION := 15
+PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
