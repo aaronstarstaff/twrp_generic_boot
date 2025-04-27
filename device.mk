f931551b01509926c88059cdc84f439e45a06f8a
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2025-04-27 17:22:11 +0200

    Added virtual_ab and vendor_ramdisk stff

--- device.mk
+++ device.mk
@@ -5,7 +5,19 @@
 #
 LOCAL_PATH := device/generic/boot
 
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from virtual AB OTA config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
-
-
