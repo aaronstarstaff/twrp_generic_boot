a0fc713ffea82695a59e7617cd2d0df884d5b579
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2025-04-27 19:13:02 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,13 @@ DEVICE_PATH := device/google/oriole
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
+# 12.1 manifest requirements
+TARGET_SUPPORTS_64_BIT_APPS := true
+TARGET_IS_64_BIT := true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
