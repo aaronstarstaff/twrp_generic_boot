3c5bc01ec826bebd3291100546f5865d4713ff9d
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2025-04-27 17:15:34 +0200

    added resetprop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,6 +82,7 @@ TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_INCLUDE_RESETPROP := true
 TW_USE_TOOLBOX := true
 TW_MAX_BRIGHTNESS := 100
 # Hack: prevent anti rollback
