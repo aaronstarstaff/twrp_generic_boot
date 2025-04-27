6f859d0fd11b9a5bf80718aab0117e64b6ee66af
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2025-04-27 17:24:40 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -21,3 +21,59 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
+
+# A/B OTA
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    odm \
+    product \
+    system \
+    system_dlkm \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
+    vendor \
+    vendor_boot
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    checkpoint_gc \
+    update_engine \
+    update_engine_client \
+    update_engine_sideload \
+    update_verifier
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# HAL Bootctrl Pixel
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-service-pixel \
+    android.hardware.boot@1.2-impl-pixel
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd 
+
+# vndservicemanager and vndservice no longer included in API 30+, however needed by vendor code.
+PRODUCT_PACKAGES += vndservicemanager
+PRODUCT_PACKAGES += vndservice
+
+
+# Build libtrusty
+PRODUCT_PACKAGES += libtrusty
+
+# Build libion
+PRODUCT_PACKAGES += \
+    libion
